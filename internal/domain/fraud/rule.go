f66b6f260ca14e77c30a399dd0bab0304b15acd2
mdeadwiler <mdeadwiler85@gmail.com>
2025-11-18 00:16:03 -0500

    Update rule.go
    Add Config blob, rule management, RuleEval Context, TransactionSummary,

--- internal/domain/fraud/rule.go
+++ internal/domain/fraud/rule.go
@@ -46,4 +46,49 @@ type Reule struct {
 	Type 	  RuleType      `json:"type"`
 	Severity   RuleSeverity  `json:"severity"`
 	Action 	  RuleAction    `json:"action"`
+
+	// Configuration - JSON blob for flexibility
+	// Example for velocity: { "max_transactions": 5, "time_window_minutes": 10, "ammount_threshold": "1000" }
+	Config 	  map[string]interface{} `json:"config"`
+
+	// Rule management
+	Enabled bool		  `json:"enabled"`
+	Version int		  `json:"version"`
+	CreatedBy uuid.UUID     `json:"created_by"`
+
+	// Timestamps
+	CreatedAt time.time   `json:"created_at"`
+	UpdatedAt time.time   `json:"updated_at"`
+	EffectedAt time.time   `json:"effected_at"`
+	ExpiredAt  *time.time  `json:"expired_at,omitempty"`
+}
+
+// RuleEvaluationContext contains all data needed to evaluate rules
+type RuleEvaluationContext struct {
+	TransactionID uuid.UUID           `json:"transaction_id"`
+	UserId uuid.UUID			  `json:"user_id"`	
+	AccountID uuid.UUID		  `json:"account_id"`
+	Amount decimal.Decimal	  `json:"amount"`
+	Currency string 		  `json:"currency"`
+	Timestamp time.Time		  `json:"timestamp"`
+
+	// Context data for different rule types
+	Location *GeoLocation
+	Device *DeviceInfo
+	Merchant *MerchantInfo
+	Paymen *PaymentInfo
+
+	// Historical data ( fetched from Redis/DB )
+	RecentTrabsactions []TransactionSummary
+	UserProfile *UserProfile
+	DeviceHistory []DeviceRecord
+}
+
+// TransactionSummary is a lightweight transaction representation for rule evaluation
+type TransactionSummary struct {
+	ID uuid.UUID		  `json:"id"`
+	Amount decimal.Decimal	  `json:"amount"`
+	Timestamp time.Time		  `json:"timestamp"`
+	Location *GeoLocation	  `json:"location,omitempty"`
+	Status string		  `json:"status"`
 }
