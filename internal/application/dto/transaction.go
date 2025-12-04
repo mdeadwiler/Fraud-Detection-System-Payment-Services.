eab607f4f1b63bfe935d5453f85cdf104581be2b
mdeadwiler <mdeadwiler85@gmail.com>
2025-12-04 15:35:33 -0500

    Update transaction.go
    added create transaction request

--- internal/application/dto/transaction.go
+++ internal/application/dto/transaction.go
@@ -11,8 +11,12 @@ import (
 // Create trasaction request represents a request to create a new transaction for detection
 
 type CreateTreansactionRequests struct {
-ExternalID string `json:"external_id:"required`
-UserID uuid.UUID `json:"user_id" validate: "required`
-AccountID uuid.UUID `json:"account_id" validate:"required`
-
+ExternalID string `json:"external_id" validate:"required"`
+UserID uuid.UUID `json:"user_id" validate:"required"`
+AccountID uuid.UUID `json:"account_id" validate:"required"`
+Type string `json:"type" validate:"required,oneof=purchase refund withdraw deposit trasfer"`
+Amount decimal.Decimal `json:"amount" validate:"required"`
+Currency string `json:"currency" validate:"required,len=3"`
+Description string `json:"description"`
 }
+
