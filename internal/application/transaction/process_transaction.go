a8f867868f131d58f3437308baf2a170a6fa9baa
mdeadwiler <mdeadwiler85@gmail.com>
2025-12-04 16:21:39 -0500

    Update process_transaction.go
    Add transaction use case to create transaction use case

--- internal/application/transaction/process_transaction.go
+++ internal/application/transaction/process_transaction.go
@@ -14,3 +14,27 @@ import (
 	"fraud-detecction-system/internal/domain/transaction"
 	)
 
+// ProcessTransactionUseCase orchestrates transaction processing with fraud detection
+// This is the main entry point for all incoming transactions 
+type ProcessTransactionUseCase struct {
+	// Domain services 
+	txService *transaction.Service
+	fraudService *fraud.Service
+
+	// Configs
+	fraudCheckTimeout time.Duration
+	enableAsync bool
+}
+
+// NewProcessTransctionUseCase creates a new use case instance
+func NewProcessTransctionUseCase(
+	txService *transaction.Service,
+	fraudeService *fraud.Service,
+) *ProcessTransactionUseCase{
+	return &ProcessTransactionUseCase{
+		txService: txService,
+		fraudService: fraudeService,
+		fraudCheckTimeout: 200 * time.Millisecond, //p99 target
+		enableAsync: false,  //Synchronous by default for correctness 
+	}
+}
