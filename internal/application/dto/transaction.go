d23408c0af9fd210097430591dcfd0bf2dc3e7ce
mdeadwiler <mdeadwiler85@gmail.com>
2025-12-04 15:57:10 -0500

    Update transaction.go
    Add merchant and payment information

--- internal/application/dto/transaction.go
+++ internal/application/dto/transaction.go
@@ -32,7 +32,7 @@ CreatedAt time.Time `json:"created_at"`
 ProcessedAt *time.Time `json:"processed_at,omitempty"`
 }
 
-//GeoLocationDTO represents geographic location data
+// GeoLocationDTO represents geographic location data
 type GeoLocation struct {
 	Latitude float64 `json:"latitude"`
 	Longitude float64 `json:"longitude"`
@@ -42,4 +42,21 @@ type GeoLocation struct {
 	IPAddress string `json:"ip_address"`
 }
 
+// MerchantDTO represents merchant information
+type MerchantDTO struct {
+	MerchantId string `json:"type"`
+	Name string `json:"name"`
+	Category string `json:"category"`
+	Country string `json:"country"`
+	RiskScore string `json:"risk_score,omitempty"`
+}
+
+// PaymentDTO for payment information
+type PaymentDTO struct {
+	Type string `json:"type"` // car, bank_transfer, wallet
+	Last4 string `json:"last4,omitempty"`
+	CardType string `json:"card_type,omitempty"`
+	BankID string `json:"bank_id,omitempty"`
+	IssuingCountry string `json:"issuing_country,omitempty"`
+}
 
